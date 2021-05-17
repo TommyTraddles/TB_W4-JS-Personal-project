aff2b45140212c967885ab8390afa130034bf6b9
TommyTraddles <jandresnova@gmail.com>
2021-05-17 15:44:37 +0200

    feat. momodal con contenido repitiendose

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 'use strict'
 
 
-// --------------------- Objeto
+
+// --------------------- Objeto ✅
 let projects = [
 	{
 		id: 1,
-		name: "mundo ácido",
+		name: "Mundo ácido",
 		urlProd: "https://tommytraddles.github.io/TheBridge_W2-CSS-mundo-acido/",
 		urlRepo: "https://github.com/TommyTraddles/TheBridge_W2-CSS-mundo-acido",
 		tags: [
@@ -19,7 +20,7 @@ let projects = [
 	},
 	{
 		id: 2,
-		name: "contactos copleros",
+		name: "Contactos copleros",
 		urlProd: "https://tommytraddles.github.io/TheBridge_W2-CSS-contactos-copleros/",
 		urlRepo: "https://github.com/TommyTraddles/TheBridge_W2-CSS-contactos-copleros",
 		tags: [
@@ -49,8 +50,8 @@ let projects = [
 	{
 		id: 4,
 		name: "Sandbox project",
-		urlProd: "",
-		urlRepo: "",
+		urlProd: "https://tommytraddles.github.io/",
+		urlRepo: "https://github.com/TommyTraddles/TommyTraddles.github.io",
 		tags: [
 			"javascript",
 			"html",
@@ -69,16 +70,20 @@ let projects = [
 
 
 
-// --------------------- Shell card
+
+
+
+// --------------------- Shell card ✅
 const gallery = document.querySelector(".gallery");
 function seeProjects (){
 	for (const project in projects) {
 		const shell_gallery_card = document.createElement('div');
 		shell_gallery_card.className = 'gallery__card';
-		shell_gallery_card.innerHTML = '<div class="gallery__card-title">' + projects[project].name + '</div>'
+		shell_gallery_card.innerHTML = 
+		  '<div class="gallery__card-title">' /* + projects[project].id + '. '*/ + projects[project].name + '</div>'
 		+ '<div class="gallery__card-buttons">'
 		+ '<button class="gallery__card-play btn--s">Play</button>'
-		+ '<button class="gallery__card-modal btn--s btn--white">Learn more</button> '
+		+ '<button class="gallery__card-modal btn--s btn--white">More info</button> '
 		+'</div>';
 		gallery.prepend(shell_gallery_card);
 	};
@@ -91,9 +96,83 @@ seeProjects();
 
 
 
-// --------------------- Shell sandbox
+
+
+
+
+
+// -------------------- Modal 
+
+// Funcionalidad
+const gallery_card_modal = document.querySelectorAll('.gallery__card-modal');
+const modal_overlay = document.querySelector('.modal__overlay');
+const modal_container = document.querySelector('.modal__container');
+const modal_close= document.querySelector('.modal__close');
+// shell
+// const modal_info = document.querySelector('.modal__info');
+// const modal = document.querySelector('.modal');
+// const modal_info_title = document.querySelector('.modal__info-title');// and urk
+// const modal_info_badges = document.querySelector('.modal__info-badges');
+// const modal_info_description = document.querySelector('.modal__info-desc');
+// const modal_info_kpi = document.querySelector('.modal__info-kpi'); // tres valores
+
+function defineModal(){
+	console.log('> DefineModal funciona')
+	
+
+	let shell_modal_card = document.createElement('div');
+	shell_modal_card.className = 'modal__info';
+	shell_modal_card.innerHTML =
+	  '<div class="modal__info-title"> <a href="' + projects[0].urlRepo + '" target="_blank"> ' + projects[0].name + ' 🔗 </a> </div>'
+	+ '<div class="modal__info-badges"> <span> '+ projects[0].tags[0] + ' </span> </span></div>' /* for each tag */
+	+ '<div class="modal__info-desc"> <p> '+projects[0].description +' </p> </div>'
+	+ '<div class="modal__info-kpi">'
+	+ 	'<div class="modal__info-kpi-hours"><div class="--number"> '+projects[0].kpiHour+' </div><div class="--footnote">hours</div></div>'
+	+ 	'<div class="modal__info-kpi-lines"><div class="--number"> '+projects[0].kpiLines+' </div><div class="--footnote">commented lines of code</div></div>'
+	+ 	'<div class="modal__info-kpi-weight"><div class="--number"> '+projects[0].kpiWeigth+' </div><div class="--footnote">file weight</div></div>'
+	+ '</div>';
+	modal_container.innerHTML = shell_modal_card;
+
+	openModal()
+}
+
+
+function openModal(){
+	console.log('> Open Modal Funciona')
+	modal_container.classList.add('modal__container--active');
+	modal_overlay.classList.add('modal__overlay--active');
+};
+function closeModal(){
+	modal_container.classList.remove('modal__container--active');
+	modal_overlay.classList.remove('modal__overlay--active');
+};
+
+
+gallery_card_modal.forEach(selector => {
+	selector.addEventListener('click', defineModal);
+}); 
+modal_close.addEventListener('click', closeModal);
+
+
+
+
+
+
+
+
+
+// --------------------- Random project ✅
+const sandbox_random = document.querySelector('.sandbox__buton');
 const sandbox_url = document.querySelector('.sandbox__iframe');
 const sandbox_name = document.querySelector('.sandbox__name');
+function randomProject (){
+	let roll_the_dice = ~~(Math.random()*projects.length);
+	let shell_sandbox_url = '<iframe src="'+projects[roll_the_dice].urlProd +'"></iframe>'
+	let shell_sandbox_name = '<p>'+ projects[roll_the_dice].name+'</p>'
+	sandbox_url.innerHTML = shell_sandbox_url;
+	sandbox_name.innerHTML = shell_sandbox_name;
+}
+sandbox_random.addEventListener('click', randomProject);
 
 
 
@@ -101,8 +180,10 @@ const sandbox_name = document.querySelector('.sandbox__name');
 
 
 
-// --------------------- Random project
-const sandbox_random = document.querySelector('.sandbox__buton');
+
+
+
+// --------------------- Shell sandbox
 
 
 
@@ -110,7 +191,10 @@ const sandbox_random = document.querySelector('.sandbox__buton');
 
 
 
-// -------------------- Sticky header
+
+
+
+// -------------------- Sticky header ✅
 const header = document.querySelector('.header');
 function stickyHeader(){
 	header.classList.toggle('sticky', window.scrollY > 0);
@@ -123,7 +207,10 @@ window.addEventListener('scroll', stickyHeader)
 
 
 
-// -------------------- Dark theme
+
+
+
+// -------------------- Dark theme ✅
 const switcher_toggle = document.querySelector('.header__switcher-toggle-switcher');
 function switchTheme() {
 	if (switcher_toggle.checked) {
@@ -140,7 +227,10 @@ switcher_toggle.addEventListener('click', switchTheme);
 
 
 
-// -------------------- Fab
+
+
+
+// -------------------- Fab ✅
 const floating_button = document.querySelector('.floating__button');
 const floating_box = document.querySelector('.floating__box');
 function openFab() {
@@ -152,37 +242,4 @@ floating_button.addEventListener('click', openFab);
 
 
 
-
-
-// -------------------- Modal 
-const gallery_card_modal = document.querySelectorAll('.gallery__card-modal');
-const modal_overlay = document.querySelector('.modal__overlay');
-const modal_container = document.querySelector('.modal__container');
-const modal_close= document.querySelector('.modal__close');
-function openModal(){
-	console.log('> Funciona el botón modal')
-	modal_container.classList.add('modal__container--active');
-	modal_overlay.classList.add('modal__overlay--active');
-};
-function closeModal(){
-	modal_container.classList.remove('modal__container--active');
-	modal_overlay.classList.remove('modal__overlay--active');
-};
-gallery_card_modal.forEach(selector => {
-	selector.addEventListener('click', openModal);
-}); 
-modal_close.addEventListener('click', closeModal);
-
-
-
-
-
-
-
-// --------------------- Shell modal
-// Shell modal
-const modal_info_title = document.querySelector('.modal__info-title');// and urk
-const modal_info_badges = document.querySelector('.modal__info-badges');
-// const modal_info_description = documentElement.querySelector('.modal__info-desc');
-const modal_info_kpi = document.querySelector('.modal__info-kpi'); // tres valores
 
