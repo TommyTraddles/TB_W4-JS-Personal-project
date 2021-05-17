a1b69df1c6eee6348292fd4962367f3920991c0f
TommyTraddles <jandresnova@gmail.com>
2021-05-17 17:20:31 +0200

    feat. modal según contenido

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let projects = [
 			"html",
 			"css"
 		],
-		description: "Sed molestie egestas ex. Sed et massa a ex tristique hendrerit. Duis ipsum erat, ornare ut mauris quis, mattis bibendum eros. Praesent sagittis justo in vestibulum egestas. Fusce dapibus, felis in dapibus molestie, urna lacus rhoncus quam, et ultrices enim nisi vitae erat. Vestibulum condimentum sodales arcu in pellentesque. ",
+		description: "Sed molestie egestas ex. Sed et massa a ex tristique hendrerit. Duis ipsum erat, ornare ut mauris quis, mattis bibendum eros. ",
 		kpiHour: 10,
 		kpiLines: 30,
 		kpiWeigth: 200
@@ -27,7 +27,7 @@ let projects = [
 			"html",
 			"css"
 		],
-		description: "Sed molestie egestas ex. Sed et massa a ex tristique hendrerit. Duis ipsum erat, ornare ut mauris quis, mattis bibendum eros. Praesent sagittis justo in vestibulum egestas. Fusce dapibus, felis in dapibus molestie, urna lacus rhoncus quam, et ultrices enim nisi vitae erat. Vestibulum condimentum sodales arcu in pellentesque. ",
+		description: "Sed molestie egestas ex. Sed et massa a ex tristique hendrerit. Duis ipsum erat, ornare ut mauris quis, mattis bibendum eros. ",
 		kpiHour: 10,
 		kpiLines: 30,
 		kpiWeigth: 200
@@ -42,7 +42,7 @@ let projects = [
 			"html",
 			"css"
 		],
-		description: "Sed molestie egestas ex. Sed et massa a ex tristique hendrerit. Duis ipsum erat, ornare ut mauris quis, mattis bibendum eros. Praesent sagittis justo in vestibulum egestas. Fusce dapibus, felis in dapibus molestie, urna lacus rhoncus quam, et ultrices enim nisi vitae erat. Vestibulum condimentum sodales arcu in pellentesque. ",
+		description: "Sed molestie egestas ex. Sed et massa a ex tristique hendrerit. Duis ipsum erat, ornare ut mauris quis, mattis bibendum eros. ",
 		kpiHour: 10,
 		kpiLines: 30,
 		kpiWeigth: 200
@@ -57,7 +57,7 @@ let projects = [
 			"html",
 			"css"
 		],
-		description: "Sed molestie egestas ex. Sed et massa a ex tristique hendrerit. Duis ipsum erat, ornare ut mauris quis, mattis bibendum eros. Praesent sagittis justo in vestibulum egestas. Fusce dapibus, felis in dapibus molestie, urna lacus rhoncus quam, et ultrices enim nisi vitae erat. Vestibulum condimentum sodales arcu in pellentesque. ",
+		description: "Sed molestie egestas ex. Sed et massa a ex tristique hendrerit. Duis ipsum erat, ornare ut mauris quis, mattis bibendum eros. ",
 		kpiHour: 10,
 		kpiLines: 30,
 		kpiWeigth: 200
@@ -72,19 +72,18 @@ let projects = [
 
 
 
-
 // --------------------- Shell card ✅
 const gallery = document.querySelector(".gallery");
-function seeProjects (){
+function seeProjects() {
 	for (const project in projects) {
 		const shell_gallery_card = document.createElement('div');
 		shell_gallery_card.className = 'gallery__card';
-		shell_gallery_card.innerHTML = 
-		  '<div class="gallery__card-title">' /* + projects[project].id + '. '*/ + projects[project].name + '</div>'
-		+ '<div class="gallery__card-buttons">'
-		+ '<button class="gallery__card-play btn--s">Play</button>'
-		+ '<button class="gallery__card-modal btn--s btn--white">More info</button> '
-		+'</div>';
+		shell_gallery_card.innerHTML =
+			'<div class="gallery__card-title">' /* + projects[project].id + '. '*/ + projects[project].name + '</div>'
+			+ '<div class="gallery__card-buttons">'
+			+ '<button class="gallery__card-play btn--s" id="'+projects[project].id+'">Play</button>'
+			+ '<button class="gallery__card-modal btn--s btn--white" id="'+projects[project].id+'" onClick="defineModal(this.id)">More info</button> '
+			+ '</div>';
 		gallery.prepend(shell_gallery_card);
 	};
 };
@@ -98,59 +97,53 @@ seeProjects();
 
 
 
-
-
-
 // -------------------- Modal 
-
-// Funcionalidad
 const gallery_card_modal = document.querySelectorAll('.gallery__card-modal');
-const modal_overlay = document.querySelector('.modal__overlay');
 const modal_container = document.querySelector('.modal__container');
-const modal_close= document.querySelector('.modal__close');
-// shell
-// const modal_info = document.querySelector('.modal__info');
-// const modal = document.querySelector('.modal');
-// const modal_info_title = document.querySelector('.modal__info-title');// and urk
-// const modal_info_badges = document.querySelector('.modal__info-badges');
-// const modal_info_description = document.querySelector('.modal__info-desc');
-// const modal_info_kpi = document.querySelector('.modal__info-kpi'); // tres valores
+const modal_info = document.querySelector('.modal__info');
+const modal_close = document.querySelector('.modal__close');
+const modal_overlay = document.querySelector('.modal__overlay');
+const gallery_card_title = document.querySelector('.gallery__card-title');
 
-function defineModal(){
-	console.log('> DefineModal funciona')
-	
 
+/* for each tag ❌ */
+
+function defineModal(id) {
 	let shell_modal_card = document.createElement('div');
 	shell_modal_card.className = 'modal__info';
 	shell_modal_card.innerHTML =
-	  '<div class="modal__info-title"> <a href="' + projects[0].urlRepo + '" target="_blank"> ' + projects[0].name + ' 🔗 </a> </div>'
-	+ '<div class="modal__info-badges"> <span> '+ projects[0].tags[0] + ' </span> </span></div>' /* for each tag */
-	+ '<div class="modal__info-desc"> <p> '+projects[0].description +' </p> </div>'
-	+ '<div class="modal__info-kpi">'
-	+ 	'<div class="modal__info-kpi-hours"><div class="--number"> '+projects[0].kpiHour+' </div><div class="--footnote">hours</div></div>'
-	+ 	'<div class="modal__info-kpi-lines"><div class="--number"> '+projects[0].kpiLines+' </div><div class="--footnote">commented lines of code</div></div>'
-	+ 	'<div class="modal__info-kpi-weight"><div class="--number"> '+projects[0].kpiWeigth+' </div><div class="--footnote">file weight</div></div>'
-	+ '</div>';
-	modal_container.innerHTML = shell_modal_card;
-
+		'<div class="modal__info-title""> <a href="' + projects[id-1].urlRepo + '" target="_blank"> ' + projects[id-1].name + ' 🔗 </a> </div>'
+		+ '<div class="modal__info-badges"> <span> ' + projects[id-1].tags[0] + ' </span> </span></div>'
+		+ '<div class="modal__info-desc"> <p> ' + projects[id-1].description + ' </p> </div>'
+		+ '<div class="modal__info-kpi">'
+		+ '<div class="modal__info-kpi-hours"><div class="--number"> ' + projects[id-1].kpiHour + ' </div><div class="--footnote">hours</div></div>'
+		+ '<div class="modal__info-kpi-lines"><div class="--number"> ' + projects[id-1].kpiLines + ' </div><div class="--footnote">commented lines of code</div></div>'
+		+ '<div class="modal__info-kpi-weight"><div class="--number"> ' + projects[id-1].kpiWeigth + ' </div><div class="--footnote">file weight</div></div>'
+		+ '</div>';
+	modal_container.appendChild(shell_modal_card); /* anunca deja de aparcer objeto  */
 	openModal()
 }
 
 
-function openModal(){
-	console.log('> Open Modal Funciona')
+// abrir y cerrar ✅
+function openModal() {
 	modal_container.classList.add('modal__container--active');
 	modal_overlay.classList.add('modal__overlay--active');
 };
-function closeModal(){
+
+
+function closeModal() {
 	modal_container.classList.remove('modal__container--active');
 	modal_overlay.classList.remove('modal__overlay--active');
+	// Revisar mejor solución
+	let modal_info = document.querySelector('.modal__info');
+	modal_container.removeChild(modal_info);
 };
 
 
-gallery_card_modal.forEach(selector => {
-	selector.addEventListener('click', defineModal);
-}); 
+// gallery_card_modal.forEach(selector => {
+// 	selector.addEventListener('click', defineModal);
+// });
 modal_close.addEventListener('click', closeModal);
 
 
@@ -165,10 +158,10 @@ modal_close.addEventListener('click', closeModal);
 const sandbox_random = document.querySelector('.sandbox__buton');
 const sandbox_url = document.querySelector('.sandbox__iframe');
 const sandbox_name = document.querySelector('.sandbox__name');
-function randomProject (){
-	let roll_the_dice = ~~(Math.random()*projects.length);
-	let shell_sandbox_url = '<iframe src="'+projects[roll_the_dice].urlProd +'"></iframe>'
-	let shell_sandbox_name = '<p>'+ projects[roll_the_dice].name+'</p>'
+function randomProject() {
+	let roll_the_dice = ~~(Math.random() * projects.length);
+	let shell_sandbox_url = '<iframe src="' + projects[roll_the_dice].urlProd + '"></iframe>'
+	let shell_sandbox_name = '<p>' + projects[roll_the_dice].name + '</p>'
 	sandbox_url.innerHTML = shell_sandbox_url;
 	sandbox_name.innerHTML = shell_sandbox_name;
 }
@@ -181,9 +174,7 @@ sandbox_random.addEventListener('click', randomProject);
 
 
 
-
-
-// --------------------- Shell sandbox
+// --------------------- Shell sandbox ❌
 
 
 
@@ -196,7 +187,7 @@ sandbox_random.addEventListener('click', randomProject);
 
 // -------------------- Sticky header ✅
 const header = document.querySelector('.header');
-function stickyHeader(){
+function stickyHeader() {
 	header.classList.toggle('sticky', window.scrollY > 0);
 }
 window.addEventListener('scroll', stickyHeader)
